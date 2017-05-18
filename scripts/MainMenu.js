104614846050799c8842ea85fa0e125a5c1aad58
BenediktEyjolfsson <t1505741@student.southstaffs.ac.uk>
2017-05-18 11:17:32 +0100

    updated .js files
    MainGame.js
    MainMenu.js
    Preloader.js

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -1,13 +1,34 @@
 BasicGame.MainMenu = function (game) { };
 
+var startButton;
+var starfield;
+var logo;
+
 BasicGame.MainMenu.prototype = {
 
 	create: function () {
-
+        //we've already loaded the assets so we'll move into the main menu
+        //Here all we are doing is playing music,adding a pictuere and a button
+        //I will modify the main menu to suit my game
+        
+        //output sky, ship, score, lives, total and start time to the screen
+        //the scrolling starfield background
+        starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
+        logo = thi.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
+        logo.anchor.setTo(0.5,0.5);
+        startButton = this.add.button((this.world / 2), (this.world.height / 2) + 50, 'startButton', this.StartGame);
+        startButton.anchor.setTo(0.5,0.5);
+        
 	},
 
 	update: function () {
 		//	Do some nice funky main menu effect here
-	}
+        
+	},
+    
+startGame: function () {
+    //start the game
+    this.game.state.start('game');
+}
 
 };
--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -7,6 +7,18 @@ BasicGame.Preloader = function (game) {
 BasicGame.Preloader.prototype = {
 
 	preload: function () {
+        //Displays a loading screen mwessage while the assets are loaded into the memory
+        this.preloaderText  this.add.text(this.world.centerX, this.world.centerY, 'loading!', {
+            fontSize: '96px',
+            fill: '#fff',
+            algin: 'center'
+        });
+        this.preloaderText.anchor.setTo(0.5,0.5);
+        //preload the images, sprites and audio assets into the memory
+        this.load.image('logo', 'assets/PhaserLogo.png');
+        this.load.image('starfield', 'assets/starfield.png');
+        this.load.image('startButton', 'assets/startButton.png');
+        this.load.image('ship', 'assets/ship.png');
         
 	},
 
@@ -15,7 +27,8 @@ BasicGame.Preloader.prototype = {
 	},
 
 	update: function () {
-
+        //
+        this.game.state.start('MainMenu');
 	}
 
 };
